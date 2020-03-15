d39635aa27f638df425c008bd77d9db63e17770e
Marek <marek.kajetan.hetman@gmail.com>
2020-03-15 18:01:14 +0100

    Git initial commit

---
+++ src/test/java/hetman/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package hetman.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
