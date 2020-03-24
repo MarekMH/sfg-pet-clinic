da651f86b06a8922e7bf41cb05a8f50b97c33cdf
Marek <marek.kajetan.hetman@gmail.com>
2020-03-24 23:00:26 +0100

    Create PetType Map Service Impl. Close #32

---
+++ pet-clinic-data/src/main/java/hetman/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package hetman.springframework.sfgpetclinic.services;
+
+import hetman.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
