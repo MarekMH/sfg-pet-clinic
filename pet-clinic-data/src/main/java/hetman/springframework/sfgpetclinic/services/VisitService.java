a0154cd821d052b0d2fdc223f908f8b003fde117
Marek <marek.kajetan.hetman@gmail.com>
2020-03-28 14:03:09 +0100

    Create Visit Map Service. Close #49

---
+++ pet-clinic-data/src/main/java/hetman/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package hetman.springframework.sfgpetclinic.services;
+
+import hetman.springframework.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
